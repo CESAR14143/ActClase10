e0e07f21f3531028adbd52907cc826b39ae93c28
CESAR14143 <89033342+CESAR14143@users.noreply.github.com>
2021-08-23 12:45:40 -0600

    Update exercise.py

--- assignments/03Ejercicio/src/exercise.py
+++ assignments/03Ejercicio/src/exercise.py
@@ -1,6 +1,25 @@
 def main() :
    #introduce el codigo
    pass
+   def operacion(x,y,z):
+   if z == "s":
+        op = x+y
+   elif z == "r":
+        op = x-y
+   elif z == "m":
+        op = x*y
+   elif z== "d":
+        op = x/y
+    return op
+
+   def main():
+    x = int(input("introducir valor 1: "))
+    y = int(input("introducir valor 2: "))
+    z = input("introducir clave  (s, r, m, d): ")
+    print("resultado: "+str(operacion(x,y,z)))
+    
+   main()  
+
 
 
 
